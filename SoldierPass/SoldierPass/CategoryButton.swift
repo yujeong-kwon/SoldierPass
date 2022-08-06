e2269cef92ad52d640ef3f8b08dc535a7d4b6668
Tori <ttang1135@naver.com>
2022-08-06 14:43:32 +0900

    메인페이지 스토리보드 작성 스토리보드 구현 및 카테고리 버튼 클래스 추가

---
+++ SoldierPass/SoldierPass/CategoryButton.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryButton.swift
+//  SoldierPass
+//
+//  Created by 방유빈 on 2022/08/06.
+//
+
+import UIKit
+
+class CategoryButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

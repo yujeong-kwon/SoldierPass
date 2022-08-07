ec84d931d7e6f51aa5e2b857b1cc8c5c8d3d92b4
Tori <ttang1135@naver.com>
2022-08-07 13:54:36 +0900

    메인 페이지 구현_디자인X

--- SoldierPass/SoldierPass/BenefitsDataController.swift
+++ SoldierPass/SoldierPass/BenefitsDataController.swift
@@ -12,6 +12,7 @@ class BenefitsDataController{
     
     init(){
         dataList = []
+        fun_loadDataFromCSV()
     }
     func fun_isEmpty()->Bool{
         if dataList.count == 0{
@@ -33,13 +34,77 @@ class BenefitsDataController{
             let data = try Data(contentsOf: url);
             let dataEncoded = String(data:data, encoding: .utf8);
             if let dataArr = dataEncoded?.components(separatedBy: "\n").map({$0.components(separatedBy: ",")}){
-                for item in dataArr{
+                for (index,item) in dataArr.enumerated(){
+                    if index == 0{
+                        continue
+                    }
                     dataList.append(item)
                 }
             }
         }catch{
             print("Error reading CSV file");
         }
+    }
+    func fun_filterData(city:String,category:[Bool])->[[String]]{
+        var data:[[String]]=[]
+        if city == "전국"{
+            data = dataList
+        }
+        else{
+            for item in dataList{
+                if item[1] == city {
+                    data.append(item)
+                }
+            }
+        }
+        var data2:[[String]]=[]
+        if category == [false,false,false,false,false,false]{
+            return data
+        }
+        
+        if category[0] == true {
+            for item in data{
+                if item[9] == "숙박" {
+                    data2.append(item)
+                }
+            }
+        }
+        if category[1] == true {
+            for item in data{
+                if item[9] == "영화" {
+                    data2.append(item)
+                }
+            }
+        }
+        if category[2] == true {
+            for item in data{
+                if item[9] == "항공" {
+                    data2.append(item)
+                }
+            }
+        }
+        if category[3] == true {
+            for item in data{
+                if item[9] == "렌트" {
+                    data2.append(item)
+                }
+            }
+        }
+        if category[4] == true {
+            for item in data{
+                if item[9] == "이사" {
+                    data2.append(item)
+                }
+            }
+        }
+        if category[5] == true {
+            for item in data{
+                if item[9] == "이용권" {
+                    data2.append(item)
+                }
+            }
+        }
+        return data2
     }
 }
 
--- SoldierPass/SoldierPass/CategoryButton.swift
+++ SoldierPass/SoldierPass/CategoryButton.swift
@@ -9,12 +9,11 @@ import UIKit
 
 class CategoryButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 8.0
+        self.backgroundColor = UIColor.lightGray
+        self.tintColor = UIColor.white
     }
-    */
 
 }

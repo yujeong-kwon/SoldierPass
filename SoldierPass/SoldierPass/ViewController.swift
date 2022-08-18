e38ea6df912484fcf3aaf96c07ca771d7e952387
Tori <ttang1135@naver.com>
2022-08-19 02:30:05 +0900

    테이블뷰 컨트롤러 위임

--- SoldierPass/SoldierPass/ViewController.swift
+++ SoldierPass/SoldierPass/ViewController.swift
@@ -40,6 +40,7 @@ class ViewController: UIViewController,UITextFieldDelegate, UIPickerViewDelegate
         tableView.rowHeight = 100
         createPickerView()
         dismissPickerView()
+        print(data)
     }
     
     //tableView
